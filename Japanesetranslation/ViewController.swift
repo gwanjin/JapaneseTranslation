68dc3580a99c1dacea0ce16613e5f3a87bf11e8e
Jeong Gwanjin <jeong.gwanjin@revamp.co.jp>
2020-01-28 20:57:14 +0900

    create project

---
+++ Japanesetranslation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Japanesetranslation
+//
+//  Created by jeong gwanjin on 2020/01/28.
+//  Copyright © 2020 jeong gwanjin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
